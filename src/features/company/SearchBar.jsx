59ed5ee51b02bc8255254b440a1d10e51dff916e
xiaohai-huang <jeromehuang18@gmail.com>
2021-03-31 01:10:28 +1000

    Support search by id.

--- src/features/company/SearchBar.jsx
+++ src/features/company/SearchBar.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Button, InputBase, makeStyles } from "@material-ui/core";
+import { useHistory } from "react-router";
 
 const useStyles = makeStyles((theme) => ({
   SearchBar: {},
@@ -16,20 +17,31 @@ const useStyles = makeStyles((theme) => ({
 
 function SearchBar() {
   const classes = useStyles();
+  const [query, setQuery] = useState("");
+  const history = useHistory();
+  const handleSubmit = () => history.push(`/news/${query}`);
 
   return (
     <div className={classes.SearchBar}>
-      <Box component="form" display="flex" flexDirection="row">
+      <Box
+        component="form"
+        display="flex"
+        flexDirection="row"
+        onSubmit={handleSubmit}
+      >
         <InputBase
           className={classes.input}
           placeholder="搜索站内资讯、视频或用户"
           fullWidth
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
         />
         <Button
           color="primary"
           variant="contained"
           disableElevation
           disableRipple
+          onClick={handleSubmit}
         >
           搜索
         </Button>
